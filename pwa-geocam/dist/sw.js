d13b56ffe84dcbf8d4f06f663f29d5575d8c14b3
Pedro Henrique Barros <pedrohenriquebarros92@hotmail.com>
2025-10-14 03:39:07 -0300

    Delete pwa-geocam directory

--- pwa-geocam/dist/sw.js
+++
@@ -1,2 +0,0 @@
-let CACHE_NAME="pwa-geocam-v1",FILES_TO_CACHE=["./","./index.html","./manifest.json"];self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(e=>e.addAll(FILES_TO_CACHE))),self.skipWaiting()}),self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(e=>Promise.all(e.map(e=>e!==CACHE_NAME&&caches.delete(e))))),self.clients.claim()}),self.addEventListener("fetch",e=>{"GET"===e.request.method&&e.respondWith(caches.match(e.request).then(t=>t||fetch(e.request)))});
-//# sourceMappingURL=sw.js.map
